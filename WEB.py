ded896c4a8289ed72039607482e7142ae4df9da6
♩😺 s𝐀𝓶Į в𝐑ᵃ𝔫𝔻 👹♘ <saimkhan76151@gmail.com>
2024-12-27 20:03:51 +0500

    Update WEB.py

--- WEB.py
+++ WEB.py
@@ -1,99 +1,171 @@
-# Logo
-logo = """
-\033[1;32m  ______ _____  _____ _____ _____ 
-\033[1;32m |  ____|  __ \|_   _|_   _/ ____|
-\033[1;32m | |__  | |__) | | |   | || |     
-\033[1;32m |  __| |  _  /  | |   | || |     
-\033[1;32m | |____| | \ \ _| |_ _| || |____ 
-\033[1;32m |______|_|  \_\_____|_____\_____|
-\033[36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-\033[1;32m[*] 𝐎𝐖𝐍𝐄𝐑      : \033[1;32m𝐄 𝐑 𝐈 𝐈 𝐂 
-\033[1;32m[*] 𝐆𝐈𝐓𝐇𝐔𝐁     : \033[1;32m𝐄 𝐑 𝐈 𝐈 𝐂  𝐄 𝐗 𝐎
-\033[1;32m[*] 𝐒𝐓𝐀𝐓𝐔𝐒     : \033[1;32m𝐏 𝐑 𝐄 𝐌 𝐈 𝐔 𝐌
-\033[1;32m[*] 𝐓𝐄𝐀𝐌       : \033[1;32m𝐎 𝐍 𝐄     𝐌 𝐀 𝐍    𝐀 𝐑 𝐌 𝐘
-\033[1;32m[*] 𝐓𝐎𝐎𝐋       : \033[1;32m𝐌 𝐔 𝐋 𝐓 𝐘   𝐓 𝐎 𝐊 𝐄 𝐍   𝐂 𝐎 𝐍 𝐕 𝐎
-\033[36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-"""
-print(logo)
+# Tool : Wasii ( Facebook cloning tool)
+# Author : Waseem Akram ( hackerwasii )
+# github : https://github.com/evildevill
+# Note : we are not responsible for any misuse
+try:
+    import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass, mechanize, requests
+    from multiprocessing.pool import ThreadPool
+    from requests.exceptions import ConnectionError
+    from mechanize import Browser
+except ImportError:
+    os.system('pip2 install requests')
+    os.system('pip2 install mechanize')
+    os.system('python2 wasi.py')
 
-import os
-import random
-import time
-import requests
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
 
-# Facebook Graph API endpoint
-thread_id = input("\033[1;32m3NT3R TH3 GR0UP + 1NB0X U1D: ")
+def exit():
+    print '[!] Exit'
+    os.sys.exit()
 
-url = f'https://graph.facebook.com/v15.0/t_{thread_id}/'
 
-# Token file paths
-token_file_paths = input("\033[1;32m3NT3R T0K3N F1L3 P4TH : ").split(',')
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!' + w[random.randint(0, len(w) - 1)] + i
 
-# Message file path
-message_file_path = input("\033[1;32m3NT3R TH3 M4SS4G3 F1L3 P4TH : ")
+    return cetak(d)
 
-# Haters name
-haters_name = input("\033[1;32m3NT3R TH3 H4TT3R N4M3 : ")
 
-# Delay between messages
-delay_between_messages = int(input("\033[1;32m3NT3R TH3 D34LY S3C0ND M4SS4G3 : "))
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x = x.replace('!%s' % i, '\x1b[%s;1m' % str(31 + j))
 
-# Read tokens from files
-access_tokens = []
-token_names = []
-for token_file_path in token_file_paths:
-    with open(token_file_path.strip(), "r") as token_file:
-        for i, token in enumerate(token_file.readlines()):
-            access_tokens.append(token.strip())
-            token_names.append(f"Token {i+1}")
+    x += '\x1b[0m'
+    x = x.replace('!0', '\x1b[0m')
+    sys.stdout.write(x + '\n')
 
-# Read messages from file
-messages = []
-with open(message_file_path, "r") as message_file:
-    messages = message_file.readlines()
 
-def get_account_name(token):
+def wasii(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+banner = '\n\x1b[1;95mTool By Hacker wasii\n We are not responsible for any \nillegal activity\n \n'
+def tik():
+    titik = [
+     '.   ', '..  ', '... ']
+    for o in titik:
+        print '\r[\xe2\x9c\x94] Logging In ' + o,
+        sys.stdout.flush()
+        time.sleep(1)
+
+
+back = 0
+id = []
+
+def tlogin():
+    os.system('clear')
+    print banner
+    username = raw_input('[\xf0\x9f\x94\x90] \x1b[1;94mTOOL USERNAME: ')
+    if username == 'hacker':
+        os.system('clear')
+        print banner
+        print '[\xe2\x9c\x93]  \x1b[1;91mTOOL USERNAME: ' + username + ' (correct)'
+    else:
+        print '[!] Invalid Username.'
+        time.sleep(1)
+        tlogin()
+    passw = raw_input('[\xf0\x9f\x94\x90]  \x1b[1;94mTOOL PASSWORD: ')
+    if passw == 'wasii':
+        os.system('clear')
+        print banner
+        print '[\xe2\x9c\x93]  \x1b[1;91mTOOL USERNAME: ' + username + ' (correct)'
+        print '[\xe2\x9c\x93]  \x1b[1;91mTOOL PASSWORD: ' + passw + '  (correct)'
+        time.sleep(2)
+    else:
+        print '[!] Invalid Password.'
+        time.sleep(1)
+        tlogin()
     try:
-        response = requests.get(f'https://graph.facebook.com/v15.0/me?access_token={token}')
-        data = response.json()
-        return data['name']
-    except Exception as e:
-        return "Unknown"
+        toket = open('login.txt', 'r')
+        os.system('python2 .wasii.py')
+    except (KeyError, IOError):
+        methodlogin()
+    else:
+        print '[!] Invalid Password'
+        time.sleep(1)
+        tlogin()
 
-def send_message(token, message, thread_id, haters_name):
+
+def methodlogin():
+    os.system('clear')
+    print banner
+    print '[1] \x1b[1;93m Login With ID/Password.'
+    print '[2]  \x1b[1;93mLogin Using Token.( No Identity Problem )'
+    print '[3]  \x1b[1;93mExit.'
+    print '      '
+    hos = raw_input('\n \x1b[1;92mChoose Option >>  ')
+    if hos == '':
+        print '[!]  Wrong Input'
+        exit()
+    elif hos == '1':
+        login()
+    elif hos == '2':
+        os.system('clear')
+        print banner
+        hosp = raw_input('[\xf0\x9f\x94\x91]  \x1b[1;91mGive Token : ')
+        tik()
+        hopa = open('login.txt', 'w')
+        hopa.write(hosp)
+        hopa.close()
+        print '\n[\xe2\x9c\x93]  \x1b[1;91mLogged In Successfully.'
+        time.sleep(1)
+        os.system('xdg-open https://www.youtube.com/channel/HackerWasii')
+        os.system('python2 .wasii.py')
+    elif hos == '0':
+        exit()
+    else:
+        print '[!] \x1b[1;91mWrong Input'
+        exit()
+
+
+def login():
+    os.system('clear')
     try:
-        message_url = f"{url}"
-        message_params = {
-            "access_token": token,
-            "message": f"{haters_name} {message}"
-        }
-        message_response = requests.post(message_url, params=message_params)
-        if message_response.status_code == 200:
-            current_time = time.strftime("%H:%M:%S")
-            print(f"""
-\033[1;32m
-✪✭═══════•『 3R11C 3X0 0NF1R3 』•═══════✭✪
-""")
-            account_name = get_account_name(token)           
-            print(f"\033[1;32m[+] M9SS9G3 S3ND T0 3R11C 3X0 => Thread ID: {thread_id} => Token: {token_names[access_tokens.index(token)]} => Account Name: {account_name} => Haters Name: {haters_name} => Message: {message} => Time: {current_time}\033[0m")
+        tb = open('login.txt', 'r')
+        os.system('python2 .wasii.py')
+    except (KeyError, IOError):
+        os.system('clear')
+        print banner
+        wasii('[\xf0\x9f\x94\x90] \x1b[1;96mLogin Your Facebook Account')
+        wasii('[\xf0\x9f\x94\x90] \x1b[1;96mDonot Use Your Personal Account')
+        wasii('[\xf0\x9f\x94\x90] \x1b[1;96mUse a New Facebook Account To Login')
+        print '-------------------------------------'
+        iid = raw_input('[+] Number/Email: ')
+        id = iid.replace(' ', '')
+        pwd = raw_input('[+] Password : ')
+        tik()
+        data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + id + '&locale=en_US&password=' + pwd + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
+        z = json.load(data)
+        if 'access_token' in z:
+            st = open('login.txt', 'w')
+            st.write(z['access_token'])
+            st.close()
+            print '\n[\xe2\x9c\x93] \x1b[1;91mLogged In Successfully.'
+            time.sleep(1)
+            os.system('xdg-open https://www.youtube.com/channe/HackerWasii')
+            os.system('clear')
+            os.system('python2 .wasii.py')
+        elif 'www.facebook.com' in z['error_msg']:
+            print '[!] \x1b[1;91mUser Must Verify Account Before Login.'
+            time.sleep(3)
+            login()
         else:
-            current_time = time.strftime("%H:%M:%S")
-            print(f"""
- \033[1;32m 
-✪✭═══════•『 3R11C 3X0 0NF1R3 』•═══════✭✪
-""")
-            print(f"\033[1;32mM3SS4G3 F9IL3D H0 GYA HAI => Thread ID: {thread_id} =>Token: {token_names[access_tokens.index(token)]} => Haters Name: {haters_name} => Message: {message} => Time: {current_time}\033[0m")
-    except Exception as e:
-        print(str(e))
+            print '[!]\x1b[1;91mNumber/User Id/ Password Is Wrong !'
+            time.sleep(1)
+            login()
 
-def process_messages_thread():
-    try:
-        while True:
-            random_token = random.choice(access_tokens)
-            random_message = random.choice(messages).strip()
-            send_message(random_token, random_message, thread_id, haters_name)
-            time.sleep(delay_between_messages)
-    except Exception as e:
-        print(str(e))
 
-process_messages_thread()
+if __name__ == '__main__':
+    tlogin()
